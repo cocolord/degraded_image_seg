13073c2fdc5a3eaad75dbeb57ffce3adccc1a6b2
LabMemNo003 <LabMemNo003@gmail.com>
2021-05-03 17:41:08 +0800

    添加一些训练config

--- configs/SETR/SETR_MLALA_DeiT_convfuse_768x768_80k_cityscapes_bs_4.py
+++ configs/SETR/SETR_MLALA_DeiT_convfuse_768x768_80k_cityscapes_bs_4.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/setr_mlala_conv.py',
+    '../_base_/models/setr_mlala_convfuse.py',
     '../_base_/datasets/cityscapes_768x768.py', '../_base_/default_runtime.py',
     '../_base_/schedules/schedule_80k.py'
 ]
---
+++ configs/swin/upernet_swin_base_patch4_window7_768x768_160k_cityscapes_bs4_sgd.py
@@ -0,0 +1,40 @@
+_base_ = [
+    '../_base_/models/upernet_swin.py', '../_base_/datasets/cityscapes_768x768.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_80k.py'
+]
+model = dict(
+    pretrained = 'work_dirs/swin_base_patch4_window7_224_22k.pth',
+    backbone=dict(
+        embed_dim=128,
+        depths=[2, 2, 18, 2],
+        num_heads=[4, 8, 16, 32],
+        window_size=7,
+        ape=False,
+        drop_path_rate=0.3,
+        patch_norm=True,
+        use_checkpoint=False
+    ),
+    decode_head=dict(
+        in_channels=[128, 256, 512, 1024],
+        num_classes=150
+    ),
+    auxiliary_head=dict(
+        in_channels=512,
+        num_classes=150
+    ))
+
+
+optimizer = dict(lr=0.002, weight_decay=0.0,
+ paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)})
+)
+
+lr_config = dict(_delete_=True, policy='poly',
+                 warmup='linear',
+                 warmup_iters=1500,
+                 warmup_ratio=1e-6,
+                 power=1.0, min_lr=0.0, by_epoch=False)
+
+# By default, models are trained on 8 GPUs with 2 images per GPU
+data=dict(samples_per_gpu=1)
