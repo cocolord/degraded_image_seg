bdde90c901d8537e8a832cce2b88a1593be8e1c1
cicolord <cicomaple@gmail.com>
2021-06-16 13:12:54 +0800

    添加cityscapes_extra数据集

---
+++ configs/_base_/datasets/cityscapes_768x768_paired_extra.py
@@ -0,0 +1,51 @@
+_base_ = './cityscapes_768x768_extra.py'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+crop_size = (768, 768)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='Resize', img_scale=(2049, 1025), ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(2049, 1025),
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+cityscapes_data_root = 'data/cityscapes/'
+cityscapes_extra_data_root = 'data/cityscapes_extra/'
+dataset_cityscapes_train = dict(
+                                type = 'CityscapesDataset',
+                                data_root=cityscapes_data_root,
+                                img_dir='leftImg8bit/train',
+                                ann_dir='gtFine/train',
+                                pipeline=train_pipeline)
+                                
+dataset_cityscapes_extra_train = dict(
+                                type = 'CityscapesExtraDataset',
+                                data_root=cityscapes_extra_data_root,
+                                img_dir='leftImg8bit',
+                                ann_dir='gtCoarse/train_extra',
+                                pipeline=train_pipeline)
+
+data = dict(
+    train=[dataset_cityscapes_extra_train, dataset_cityscapes_train],
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
